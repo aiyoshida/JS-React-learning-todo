da16da3edac5718ec79ed11866dcfc1e20e477dc
Ai Yoshida <68318468+aiyoshida@users.noreply.github.com>
2025-03-28 16:45:38 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ const onClickAdd = () => {
   //inputに書かれたテキストを取得して、liに反映されるコード
   //.valueをつけることで、inputのテキストの内容を取得できる。
   const inputText = document.getElementById("add-text").value;
-  alert(inputText);
-};
 
+  //取得した後にinputのを空にする
+  document.getElementById("add-text").value = "";
+
+  //liタグをJSで作成する
+  const li = document.createElement("li");
+  console.log(li);
+  console.log("Hello");
+  console.log("Hello");
+  console.log("Hello");
+};
 //「追加」ボタンで
 //document.getElementByIdはhtmlからid名を取得する
 //addEventListenerは、2つの引数を取る。1つ目は、イベントの種類、2つ目はどのアクションか
